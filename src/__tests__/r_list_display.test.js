d47f83a4c53415ea36d1dc5efbf99263e14dfbce
beijmn <bens_fulcrum@yahoo.com>
2023-12-30 12:43:02 -0500

    Configuring tests

--- src/__tests__/r_list_display.test.js
+++ src/__tests__/r_list_display.test.js
@@ -2,6 +2,8 @@ import '@testing-library/jest-dom';
 import R_LIST_DISPLAY from '../r_list_display.js';
 import { render, screen, cleanup } from '@testing-library/react';
 
+//	screen.debug();
+
 afterEach(() => {
 	cleanup();
 });
@@ -15,26 +17,44 @@ var contacts = [{
 	em_body: "body"
 }];
 
-function my_func() {
-	return (<div>return value</div>);
-}
+const my_func = jest.fn();
 
-test("should render", () => {
-	render(<R_LIST_DISPLAY click_select_email={my_func} items={contacts} title="" />);
-
-//	screen.debug();
+test("UT r_list_display with null contacts value", () => {
+	const bad_contacts = null;
+	render(<R_LIST_DISPLAY click_select_email={my_func} items={bad_contacts} title="" />);
 	const elem = screen.getByTestId("r_list_display-test");
 	expect(elem).toBeInTheDocument();
 })
 
-test("check button functionality", () => {
-	render(<R_LIST_DISPLAY click_select_email={my_func} items={contacts} title="" />);
+test("UT r_list_display with incorrect contacts type", () => {
+	const bad_contacts = "not an array";
+	render(<R_LIST_DISPLAY click_select_email={my_func} items={bad_contacts} title="" />);
+	const elem = screen.getByTestId("r_list_display-test");
+	expect(elem).toBeInTheDocument();
+})
 
+test("UT r_list_display main element", () => {
+	render(<R_LIST_DISPLAY click_select_email={my_func} items={contacts} title="" />);
+	const elem = screen.getByTestId("r_list_display-test");
+	expect(elem).toBeInTheDocument();
+})
+
+test("UT r_list_display button functionality", () => {
+	render(<R_LIST_DISPLAY click_select_email={my_func} items={contacts} title="" />);
 	const elem = screen.getByTestId("r_list_display-button-9");
 	expect(elem).toBeInTheDocument();
-
 	expect(elem).toHaveTextContent("testfrom@yahoo.com");
 	expect(elem).toHaveTextContent("testto@yahoo.com");
 	expect(elem).toHaveTextContent("the subject");
 	expect(elem).toHaveTextContent("date received");
+})
+
+test("IT between elements in R_LIST_DISPLAY component", () => {
+	render(<R_LIST_DISPLAY click_select_email={my_func} items={contacts} title="" />);
+	const main_element = screen.getByTestId("r_list_display-test");
+	const button_element = screen.getByTestId("r_list_display-button-9");
+	expect(main_element).toBeInTheDocument();
+	expect(button_element).toBeInTheDocument();
+	button_element.click();
+	expect(my_func).toHaveBeenCalled();
 })
---
+++ src/__tests__/string_spacing.test.js
@@ -0,0 +1,32 @@
+import '@testing-library/jest-dom';
+import STRING_SPACING from "../string_spacing.js";
+
+test("UI string_spacing truncate value", () => {
+	const disp = new STRING_SPACING();
+	// total, spacing between, string_len
+	disp.set_spacing([3, 0, 1, 1, 1]);
+	const elemen_render = disp.spacing_display("a", "b", "cccccccc");
+	const text_content = elemen_render.props.children.props.children;
+	expect(text_content).toMatch("abc");
+})
+
+/*
+describe('Component testing', () => {
+  test('Component', () => {
+    const { container } = render(<Component>I am a message</Component>);
+
+    const {firstChild} = container 
+
+    expect(firstChild).toMatchSnapshot();
+  });
+});
+*/
+
+test("UI string_spacing pad value", () => {
+	const disp = new STRING_SPACING();
+	// total, spacing between, string_len
+	disp.set_spacing([10, 0, 1, 1, 1]);
+	const elemen_render = disp.spacing_display("a", "b", "c");
+	const text_content = elemen_render.props.children.props.children;
+	expect(text_content).toMatch("abc       ");
+})
